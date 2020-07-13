9a6ebb64944e2886e9edec3f7bc8f45d207fbcc3
Martín <32708787+MartinSobel@users.noreply.github.com>
2020-07-13 18:05:32 -0300

    chart js

--- functions.js
+++ functions.js
@@ -1,3 +1,26 @@
-$( function() {
+$.ajax({
+  url: 'https://api.thevirustracker.com/free-api?countryTimeline=US',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+  }
+});
+
+$(function() {
     $(".draggable").draggable();
-  } );
+});
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+    options: {}
+});
