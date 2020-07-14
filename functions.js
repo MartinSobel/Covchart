2eca599ff3e9519cd11c56d6104b1111b6234241
Martín <32708787+MartinSobel@users.noreply.github.com>
2020-07-14 00:11:27 -0300

    getting info from API and draggable curve with chart.js

--- functions.js
+++ functions.js
@@ -1,26 +1,72 @@
-$.ajax({
-  url: 'https://api.thevirustracker.com/free-api?countryTimeline=US',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
+const apiUrl = 'https://api.covid19api.com/dayone/country/south-africa';
+async function getData(){
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const arr = data.reduce((acc, cur) => cur.Confirmed ? [...acc, cur.Confirmed] : acc, []);
+  return arr;
+}
+
+var res = [];
+getData().then( val => {
+  res.push(val);
+})  
+
+console.log(res);
+
+
+// reemplazar val por los valores del array de la api
+const val = [0, 10, 5, 2, 20, 30, 45];
+console.log(val);
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+      labels: ['', '', '', '', '', '', ''],
+      datasets: [{
+          label: '',
+          backgroundColor: 'rgb(255, 255, 255)',
+          borderColor: 'rgb(0, 0, 0)',
+          data: val
+      }]
+  },
+  options: {
+    elements: {
+      point:{
+          radius: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+            display:false
+        }
+      }],
+      yAxes: [{
+          ticks: {
+              display: false
+          },
+          gridLines: {
+            display:false
+        }   
+      }]
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+        callbacks: {
+          label: function(tooltipItem) {
+                  return tooltipItem.yLabel;
+          }
+        }
+    }
   }
 });
 
-$(function() {
-    $(".draggable").draggable();
-});
+chart.canvas.parentNode.style.width = '500px';
+chart.canvas.parentNode.style.height = '300px';
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-    options: {}
+//jQuery
+$(function() {
+  $(".draggable").draggable();
 });
